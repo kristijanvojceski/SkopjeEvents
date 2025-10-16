2352afc39f9d7d5c0f32351198e4665d1218660f
kristijanvojceski <94367314+kristijanvojceski@users.noreply.github.com>
2025-10-16 14:52:25 +0200

    email confirmation

--- client/src/features/account/LoginForm.tsx
+++ client/src/features/account/LoginForm.tsx
@@ -6,13 +6,17 @@ import { Box, Button, Paper, Typography } from "@mui/material";
 import { LockOpen } from "@mui/icons-material";
 import TextInput from "../../app/shared/components/TextInput";
 import { Link, useLocation, useNavigate } from "react-router";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function LoginForm() {
-  const { loginUser } = useAccount();
+  const [notVerified, setNotVerified] = useState(false);
+  const { loginUser, resendConfirmationEmail } = useAccount();
   const navigate = useNavigate();
   const location = useLocation();
   const {
     control,
+    watch,
     handleSubmit,
     formState: { isValid, isSubmitting },
   } = useForm<LoginScheme>({
@@ -20,10 +24,27 @@ export default function LoginForm() {
     resolver: zodResolver(loginScheme),
   });
 
+  const email = watch("email");
+
+  const handleResendEmail = async () => {
+    try {
+      await resendConfirmationEmail.mutateAsync({email});
+      setNotVerified(false);
+    } catch (error) {
+      console.log(error);
+      toast.error("Problem sending email please check email address");
+    }
+  };
+
   const onSubmit = async (data: LoginScheme) => {
     await loginUser.mutateAsync(data, {
       onSuccess: () => {
         navigate(location.state?.from || "/activities");
+      },
+      onError: (error) => {
+        if (error.message === "NotAllowed") {
+          setNotVerified(true);
+        }
       },
     });
   };
@@ -71,17 +92,32 @@ export default function LoginForm() {
       >
         Login
       </Button>
-      <Typography sx={{ textAlign: "center" }}>
-        Don't have an account?
-        <Typography
-          sx={{ ml: 2 }}
-          component={Link}
-          to="/register"
-          color="primary"
-        >
-          Sign up
+      {notVerified ? (
+        <Box display="flex" flexDirection="column" justifyContent="center">
+          <Typography textAlign="center" color="error">
+            Your email has not been verified. You can click the button to
+            re-send the verification email
+          </Typography>
+          <Button
+            disabled={resendConfirmationEmail.isPending}
+            onClick={handleResendEmail}
+          >
+            Re-send email link
+          </Button>
+        </Box>
+      ) : (
+        <Typography sx={{ textAlign: "center" }}>
+          Don't have an account?
+          <Typography
+            sx={{ ml: 2 }}
+            component={Link}
+            to="/register"
+            color="primary"
+          >
+            Sign up
+          </Typography>
         </Typography>
-      </Typography>
+      )}
     </Paper>
   );
 }
